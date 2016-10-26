c19fb83552b7c37d6e00367f0f510cba572f758c
Tuan Truong <tuan188@gmail.com>
2016-10-26 16:28:45 +0700

    Init project

---
+++ MGTextPlus/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MGTextPlus
+//
+//  Created by Tuan Truong on 10/26/16.
+//  Copyright © 2016 Tuan Truong. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
