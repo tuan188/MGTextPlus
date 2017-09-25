c8cd0cdc0555ef33c50fa0c6acae2cf5e271104f
Frédéric Ruaudel <fred@h2g.io>
2017-09-25 17:05:29 +0200

    - Implement move lines up and down

--- Line/SourceEditorCommand.swift
+++ Line/SourceEditorCommand.swift
@@ -67,6 +67,41 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
             invocation.buffer.selections.setArray([lineSelection])
         }
         
+        func notReachingTop() -> Bool {
+            return textRange.start.line > 0
+        }
+        
+        func notReachingBottom() -> Bool {
+            return textRange.end.line < (invocation.buffer.lines.count - 1)
+        }
+        
+        func moveLine(direction: CommandDirection) {
+            // easiest way to move a selection is just to remove the line
+            // after it and insert it back before it or vice versa depending
+            // on the direction
+            var lineToRemoveAfterSelection: Int
+            var lineWhereInsertingBeforeSelection: Int
+            let lineSelection = XCSourceTextRange()
+            switch direction {
+            case .up:
+                guard notReachingTop() else { return }
+                lineToRemoveAfterSelection = textRange.start.line - 1
+                lineWhereInsertingBeforeSelection = textRange.end.line
+                lineSelection.start = XCSourceTextPosition(line: textRange.start.line - 1, column: textRange.start.column)
+                lineSelection.end = XCSourceTextPosition(line: textRange.end.line - 1, column: textRange.end.column)
+            case .down:
+                guard notReachingBottom() else { return }
+                lineToRemoveAfterSelection = textRange.end.line + 1
+                lineWhereInsertingBeforeSelection = textRange.start.line
+                lineSelection.start = XCSourceTextPosition(line: textRange.start.line + 1, column: textRange.start.column)
+                lineSelection.end = XCSourceTextPosition(line: textRange.end.line + 1, column: textRange.end.column)
+            }
+            let textForLineAfterSelection = invocation.buffer.lines.object(at: lineToRemoveAfterSelection)
+            invocation.buffer.lines.removeObject(at: lineToRemoveAfterSelection)
+            invocation.buffer.lines.insert(textForLineAfterSelection, at: lineWhereInsertingBeforeSelection)
+            invocation.buffer.selections.setArray([lineSelection])
+        }
+        
         func firstClassName() -> String? {
             let pattern = "class ([^:<{ ]+)"
             for line in invocation.buffer.lines as! [String] {
@@ -82,6 +117,10 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         switch invocation.commandIdentifier {
         case bundleIdentifier + ".DeleteLine":
             deleteLines(indexSet: indexSet)
+        case bundleIdentifier + ".MoveLineUp":
+            moveLine(direction: .up)
+        case bundleIdentifier + ".MoveLineDown":
+            moveLine(direction: .down)
         case bundleIdentifier + ".DuplicateLineUp":
             duplicateLine(direction: .up)
         case bundleIdentifier + ".DuplicateLineDown":
